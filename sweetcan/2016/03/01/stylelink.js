b7917cfb7a209ec0ba41bcb63402ed1153cf18cb
sweetcan <329390143@qq.com>
2016-03-20 17:49:50 +0800

    Update stylelink.js

--- sweetcan/2016/03/01/stylelink.js
+++ sweetcan/2016/03/01/stylelink.js
@@ -1,4 +1,4 @@
-d3.text('https://bigdata-mindstorms.github.io/d3-playground/amarande/2016/03/04/background.css', 
+d3.text('https://bigdata-mindstorms/d3-playground/blob/gh-pages/sweetcan/2016/03/01/style.css', 
 function(e,d){
    d3.select('head').append('style').html(d);
    })
