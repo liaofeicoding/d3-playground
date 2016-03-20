4b11df9e3e94bcb4244951f3e50d1bc5dd27fe33
amarande <amarande@users.noreply.github.com>
2016-03-20 13:28:00 -0400

    Create nav.js

---
+++ amarande/2016/03/20/nav.js
@@ -0,0 +1,24 @@
+
+function nav(container,data){
+  
+  var container = container.append('nav')
+    .attr('class', 'navbar navbar-inverse navbar-fixed-top')
+    .append('div').attr('class', 'container');
+  var container_header = container.append('div').attr('class','navbar-header');
+  
+  container_header.append('a')
+                  .attr('class','navbar-home')
+                  .attr('href',data.home.url)
+                  .html(data.home.html);
+                     
+  container.append('div')
+           .attr('id','navbar')
+           .attr('class','collapse navbar-collapde')
+           .selectAll('li')    
+           .data(data.nav)
+           .enter()
+           .append('li')
+           .append('a')
+           .attr('href',function(d){return d.nav.url})
+           .html(function(d){return d.nav.html});
+  }
