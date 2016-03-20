ab7244e23a6ac3060d20eec86b00d27ed8053401
ontouchstart <ontouchstart@gmail.com>
2016-03-20 17:40:29 -0400

    add mobile button

--- amarande/2016/03/20/nav.js
+++ amarande/2016/03/20/nav.js
@@ -5,11 +5,26 @@ function nav(container,data){
     .attr('class', 'navbar navbar-inverse navbar-fixed-top')
     .append('div').attr('class', 'container');
   var container_header = container.append('div').attr('class','navbar-header');
+
+  var button = navbar_header.append('button')
+      .attr('type', 'button')
+      .attr('class', 'navbar-toggle collapsed')
+      .attr('data-toggle', 'collapse')
+      .attr('data-target', '#navbar')
+      .attr('aria-expanded', 'false')
+      .attr('aria-controls', 'navbar');
+  button.append('span').attr('class', 'sr-only').html('Toggle navigation');
+  button.append('span').attr('class', 'icon-bar');
+  button.append('span').attr('class', 'icon-bar');
+  button.append('span').attr('class', 'icon-bar');
+
   
   container_header.append('a')
                   .attr('class','navbar-brand')
                   .attr('href',data.home.url)
                   .html(data.home.html);
+
+
                      
   container.append('div')
            .attr('id','navbar')
