2886003893d9a65971f3e0a950a46279e7726220
Sam Liu <ontouchstart@gmail.com>
2016-03-22 17:19:06 -0400

    Update main.js

--- ontouchstart/2016/03/22/main.js
+++ ontouchstart/2016/03/22/main.js
@@ -1,8 +1,14 @@
 function flex_center_test() {
   var body = d3.select('body').html('');
-  var container = body.append('div').attr('id', 'container');
-  var h1 = container.append('h1').html('hello world');
+  var container = body.append('div')
+    .style('width', '100%')
+    .style('height', '100%')
+    .style('background', 'white');
+  var h1 = container.append('h1')
+    .html('hello world');
   flex_center(container);
 }
 
-load_js('flex_center.js', flex_center_test);
+if(location.hash === '#flex_center_test') {
+  load_js('flex_center.js', flex_center_test);
+}
