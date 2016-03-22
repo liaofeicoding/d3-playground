843dedbbec4425729d767f1de851159cc8e59bec
Sam Liu <ontouchstart@gmail.com>
2016-03-22 16:31:31 -0400

    Update main.js

--- ontouchstart/2016/03/22/main.js
+++ ontouchstart/2016/03/22/main.js
@@ -1,4 +1,11 @@
+function flex_center(container) { 
+  container.style('display', 'flex);
+  container.style('justify-content', 'center');
+  container.style('align-items', 'center');
+}
+
 console.log(d3);
 var container = d3.select('div#container');
 var [w, h] = [container.style('width'), container.style('height')].map(parseFloat);
 console.log(w, h);
+var h1 = container.append('h1').html('hello world');
