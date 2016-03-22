f662075db2b3a5ee6d537cd1bd2fe4c601e91c0e
Sam Liu <ontouchstart@gmail.com>
2016-03-22 19:11:27 -0400

    Update main.js

--- ontouchstart/2016/03/22/main.js
+++ ontouchstart/2016/03/22/main.js
@@ -6,9 +6,21 @@ function test_flex_center_center() {
     .style('width', '100%')
     .style('height', '100%')
     .style('background', 'white');
+  flex_center(container);
   var h1 = container.append('h1')
     .html('hello world');
-  flex_center_center(container);
+}
+
+function test_flex_bottom_center() {
+  body.html('');
+  var container = body.append('div')
+    .style('width', '100%')
+    .style('height', '100%')
+    .style('background', 'white');
+  flex_center(container);
+  var h1 = container.append('h1')
+    .style('align-self', 'flex-end')
+    .html('hello world');
 }
 
 function init() {
@@ -19,13 +31,20 @@ function init() {
     .append('a')
     .attr('href', '#test_flex_center_center')
     .html('Test flex_center_center');
+  ul.append('li')
+    .append('a')
+    .attr('href', '#test_flex_bottom_center')
+    .html('Test flex_bottom_center');
 }
 
 function load () {
   switch (location.hash) {
     case '#test_flex_center_center': 
-      load_js('flex_center_center.js', test_flex_center_center);
+      load_js('flex_center.js', test_flex_center_center);
       break;
+    case '#test_flex_bottom_center': 
+      load_js('flex_center.js', test_flex_bottom_center);
+      break;  
     default: 
       init();
   }
