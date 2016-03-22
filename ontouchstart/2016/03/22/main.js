ffc5090f24de0af3499afae0b5c7407a24853aaa
Sam Liu <ontouchstart@gmail.com>
2016-03-22 19:24:38 -0400

    Update main.js

--- ontouchstart/2016/03/22/main.js
+++ ontouchstart/2016/03/22/main.js
@@ -1,6 +1,19 @@
 var head = d3.select('head');
 var body = d3.select('body');
 head.select('title').html('Flex Test')
+
+function test_flex_top_center() {
+  body.html('');
+  var container = body.append('div')
+    .style('width', '100%')
+    .style('height', '100%')
+    .style('background', 'white');
+  flex_center(container);
+  var h1 = container.append('h1')
+    .style('align-self', 'flex-start')
+    .html('hello world');
+}
+
 function test_flex_center_center() {
   body.html('');
   var container = body.append('div')
@@ -31,6 +44,10 @@ function init() {
   var ul = body.append('ul');
   ul.append('li')
     .append('a')
+    .attr('href', '#flex_top_center')
+    .html('flex_top_center');
+  ul.append('li')
+    .append('a')
     .attr('href', '#flex_center_center')
     .html('flex_center_center');
   ul.append('li')
@@ -41,6 +58,10 @@ function init() {
 
 function load () {
   switch (location.hash) {
+    case '#flex_top_center': 
+      load_js('flex_center.js', test_flex_top_center);
+      break;
+
     case '#flex_center_center': 
       load_js('flex_center.js', test_flex_center_center);
       break;
