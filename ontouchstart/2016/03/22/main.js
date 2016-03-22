9892326f08e50f6814bc79a526a2b1f4277d8379
Sam Liu <ontouchstart@gmail.com>
2016-03-22 16:31:42 -0400

    Update main.js

--- ontouchstart/2016/03/22/main.js
+++ ontouchstart/2016/03/22/main.js
@@ -1,5 +1,5 @@
 function flex_center(container) { 
-  container.style('display', 'flex);
+  container.style('display', 'flex');
   container.style('justify-content', 'center');
   container.style('align-items', 'center');
 }
