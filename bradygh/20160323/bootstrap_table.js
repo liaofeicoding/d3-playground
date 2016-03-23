aeb266048b7d32f2d8147b489d43853a26fe5b7f
bradygh <brady.zhou@live.cn>
2016-03-23 08:45:25 +0800

    Create bootstrap_table.js

---
+++ bradygh/20160323/bootstrap_table.js
@@ -0,0 +1,4 @@
+d3.select('head').append('link').attr({"rel":"stylesheet","href":"//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css"});
+d3.select('head').append('link').attr({"rel":"stylesheet","href":"//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"});
+d3.select('head').append('script').attr({"src":"//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"});
+d3.select('head').append('script').attr({"src":"//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"});
