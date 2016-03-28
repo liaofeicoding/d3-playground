b93f7e624422824b7fd142e74d3779d0421aa688
Kyle Han <kyhan316@gmail.com>
2016-03-28 06:45:12 -0400

    Update nav_function.js

--- LivingProgram/2016/03/28/nav_function.js
+++ LivingProgram/2016/03/28/nav_function.js
@@ -1,1 +1,38 @@
+function nav(c, data) {
+  var container = c.append('nav')
+    .attr('class', 'navbar navbar-inverse navbar-fixed-top')
+    .append('div').attr('class', 'container');
+  var navbar_header = container.append('div').attr('class', 'navbar-header');
 
+  var button = navbar_header.append('button')
+      .attr('type', 'button')
+      .attr('class', 'navbar-toggle collapsed')
+      .attr('data-toggle', 'collapse')
+      .attr('data-target', '#navbar')
+      .attr('aria-expanded', 'false')
+      .attr('aria-controls', 'navbar');
+  button.append('span').attr('class', 'sr-only').html('Toggle navigation');
+  button.append('span').attr('class', 'icon-bar');
+  button.append('span').attr('class', 'icon-bar');
+  button.append('span').attr('class', 'icon-bar');
+
+  navbar_header.append('a')
+    .attr('class', 'navbar-brand')
+    .attr('href', data.home.url)
+    .html(data.home.html);
+
+  container.append('div')
+    .attr('id', 'navbar')
+    .attr('class', 'collapse navbar-collapse')
+    .append('ul')
+    .attr('class', 'nav navbar-nav')
+    .selectAll('li')
+    .data(data.nav)
+    .enter()
+    .append('li')
+    .append('a')
+    .attr('href', function (d) { return d.url })
+    .html(function (d) { return d.html; });
+  return container;
+}
+    
