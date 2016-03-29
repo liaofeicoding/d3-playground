f52617e7603f1c8af29bab8650e0874437d68a84
bradygh <brady.zhou@live.cn>
2016-03-29 09:02:11 +0800

    Update external_script.js

--- bradygh/20160329_1/external_script.js
+++ bradygh/20160329_1/external_script.js
@@ -1,6 +1,6 @@
 ---
 attr:
   charset: 'utf-8'
-  src: //bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/03/05/background.js
+  src: //bigdata-mindstorms.github.io/d3-playground/bradygh/20160329_1/background.js
 ---
 d3.select('head').append('script').attr({{ page.attr | jsonify }});
