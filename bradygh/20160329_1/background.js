9c4f189223e5c94646ad078a4daabe9a10c303c9
bradygh <brady.zhou@live.cn>
2016-03-29 09:00:35 +0800

    Update background.js

--- bradygh/20160329_1/background.js
+++ bradygh/20160329_1/background.js
@@ -1,6 +1,6 @@
 ---
 attr:
   rel: stylesheet
-  href: //bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/03/05/background.css
+  href: //bigdata-mindstorms.github.io/d3-playground/bradygh/20160329_1/background.css
 ---
 d3.select('head').append('link').attr({{ page.attr | jsonify }});
