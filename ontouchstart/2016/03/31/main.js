b098386c9ca9bf1182433226cb73c5d4a9045221
Sam Liu <ontouchstart@gmail.com>
2016-03-31 13:39:45 +0000

    hello world
    d3.version = 3.5.16

--- ontouchstart/2016/03/31/main.js
+++ ontouchstart/2016/03/31/main.js
@@ -1,4 +1,15 @@
 window.addEventListener('load', function () {
   var d3 = this.d3;
-  console.log(d3);
+  var fullscreen = d3.select('body').append('div')
+    .style('display', 'flex')
+    .style('width', '100%')
+    .style('height', '100%')
+    .style('background', 'white')
+    .style('justify-content', 'center')
+    .style('align-items', 'center');
+
+  var container = fullscreen.append('div'); 
+  container.append('h1').html('hello world');
+  var version = container.append('pre');
+  version.html(`d3.version = ${d3.version}`);
 });
