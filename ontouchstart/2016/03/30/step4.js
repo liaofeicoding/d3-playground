b0eec98f5c37be838650efc6a87d6f4216fed55a
Sam Liu <ontouchstart@gmail.com>
2016-03-30 22:00:41 -0400

    step 4: activate

--- ontouchstart/2016/03/30/step4.js
+++ ontouchstart/2016/03/30/step4.js
@@ -29,7 +29,9 @@ navbar_button.append('span').attr('class', 'icon-bar').html('');
 navbar_button.append('span').attr('class', 'icon-bar').html('');
 navbar_button.append('span').attr('class', 'icon-bar').html('');
 
-navbar_header.append('a').attr('class', 'navbar-brand').attr('href', '#').html('D3-Playground');
+navbar_header.append('a').attr('class', 'navbar-brand').attr('href', '#').html('D3-Playground').on('click', function () {
+  d3.selectAll('#navbar li').classed('active', false);
+});
 
 var navbar = nav_container.append('div').attr('id', 'navbar').attr('class', 'navbar-collapse collapse');
 
@@ -58,36 +60,13 @@ var navbar_ul_right = navbar.append('ul').attr('class', 'nav navbar-nav navbar-r
 navbar_ul_right.append('li').append('a').attr('href', '#about').html('About');
 navbar_ul_right.append('li').append('a').attr('href', '#contact').html('Contact');
 
+d3.selectAll('#navbar li').on('click', activate);
+
+function activate () { 
+  d3.selectAll('#navbar li').classed('active', false); 
+  d3.select(this).classed('active', true); 
+}
+
 `);
-
-
-
-/*
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
-            <li><a href="../navbar-fixed-top/">Fixed top</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-*/
 
 }
