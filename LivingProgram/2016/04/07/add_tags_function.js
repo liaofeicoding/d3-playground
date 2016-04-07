0acf6d06ee8808c13b5d1d57fdf3025585aa8d93
Kyle Han <kyhan316@gmail.com>
2016-04-07 08:01:07 -0400

    Create add_tags_function.js

---
+++ LivingProgram/2016/04/07/add_tags_function.js
@@ -0,0 +1,20 @@
+//title, meta description, meta keywords
+function title_tag(title) {
+  d3.select('title').remove();
+  d3.select('head').append('title').html(title);
+}
+function meta_description_tag(description) {
+  d3.select('head').append('meta').attr("name", "description").attr("content", description);
+}
+function meta_keyword_tag(keywords) {
+  d3.select('head').append('meta').attr("name", "keywords").attr("content", keywords);
+}
+
+function load_tags () {
+  title_tag("Forever Soul Sisters The Indigo Adventure");
+  meta_description_tag("The Forever Soul Sisters is an enthralling book following the life of two girls and their life as forever SoulSisters.");
+  meta_keyword_tag("book, soulsisters, forever");
+}
+
+
+window.addEventListener('load', load_tags);
