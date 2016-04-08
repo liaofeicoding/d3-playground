91119f88dc160f60af46a228e21383ecf3b59891
Kyle Han <kyhan316@gmail.com>
2016-04-07 20:24:51 -0400

    Changed to satisfy first problem
    I removed all of the previous functions and instead have a giant function. This function should be linked to the yaml data file allowing the clients to input data.

--- LivingProgram/2016/04/07/add_tags_function.js
+++ LivingProgram/2016/04/07/add_tags_function.js
@@ -1,20 +1,12 @@
-//title, meta description, meta keywords
-function title_tag(title) {
+function add_tags(data) {
+  //title
   d3.select('title').remove();
-  d3.select('head').append('title').html(title);
-}
-function meta_description_tag(description) {
-  d3.select('head').append('meta').attr("name", "description").attr("content", description);
-}
-function meta_keyword_tag(keywords) {
-  d3.select('head').append('meta').attr("name", "keywords").attr("content", keywords);
-}
+  d3.select('head').append('title').html(function (d) { return d.homepage.title; });
 
-function load_tags () {
-  title_tag("Forever Soul Sisters The Indigo Adventure");
-  meta_description_tag("The Forever Soul Sisters is an enthralling book following the life of two girls and their life as forever SoulSisters.");
-  meta_keyword_tag("book, soulsisters, forever");
+  //meta description
+  d3.select('head').append('meta').attr("name", "description").attr("content", function (d) { return d.homepage.description; });
+  
+  //meta keywords
+  d3.select('head').append('meta').attr("name", "keywords").attr("content", function (d) { return d.homepage.keywords; });
+
 }
-
-
-window.addEventListener('load', load_tags);
