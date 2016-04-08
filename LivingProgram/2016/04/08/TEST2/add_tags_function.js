6afa0e7e4a52f7dfec29b749156fdf790b16bf9d
Kyle Han <kyhan316@gmail.com>
2016-04-08 06:13:44 -0400

    Create add_tags_function.js

---
+++ LivingProgram/2016/04/08/TEST2/add_tags_function.js
@@ -0,0 +1,18 @@
+function add_tags(data) {
+  //title
+  d3.select('title').remove();
+  d3.select('head').append('title').html(data.title);
+
+  //meta description
+  d3.select('head').append('meta').attr("name", "description").attr("content", data.description);
+  
+  //meta keywords
+  d3.select('head').append('meta').attr("name", "keywords").attr("content", data.keywords);
+
+  //all og tags
+  d3.select('head')
+  .selectAll('meta').data(data.ogtags).enter()
+  .append('meta')
+  .attr("property", function (d) { return d.property; })
+  .attr("content", function (d) { return d.content; });
+}
