08a4d100a746672e422fc5e5fe4af37765f52b71
Kyle Han <kyhan316@gmail.com>
2016-04-07 20:33:35 -0400

    Changed homepage to data

--- LivingProgram/2016/04/07/add_tags_function.js
+++ LivingProgram/2016/04/07/add_tags_function.js
@@ -1,12 +1,12 @@
 function add_tags(data) {
   //title
   d3.select('title').remove();
-  d3.select('head').append('title').html(function (d) { return d.homepage.title; });
+  d3.select('head').append('title').html(function (d) { return d.data.title; });
 
   //meta description
-  d3.select('head').append('meta').attr("name", "description").attr("content", function (d) { return d.homepage.description; });
+  d3.select('head').append('meta').attr("name", "description").attr("content", function (d) { return d.data.description; });
   
   //meta keywords
-  d3.select('head').append('meta').attr("name", "keywords").attr("content", function (d) { return d.homepage.keywords; });
+  d3.select('head').append('meta').attr("name", "keywords").attr("content", function (d) { return d.data.keywords; });
 
 }
