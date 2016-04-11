32d936010094001b36d873a7d45680d3aaa1a99e
IntelligentDataMiner <ethan.c.kwok@gmail.com>
2016-04-10 21:14:06 -0400

    Rename Array.js to Array2.js

---
+++ IntelligentDataMiner/2016/03/14/Array2.js
@@ -0,0 +1,18 @@
+title="Menu"
+d3.select("title").html(title);
+d3.select("body.append("h1").html(title);
+
+function print_array(array){
+  var ul = d3.select('body').append('ul');
+  array.forEach(function(item, index,array) {
+  ul.append('li').html(item).attr('class','item-' +index);
+  console.log(array)
+});
+}
+
+menu1=["Salad","Soup","Steak","Cake"];
+print_array(menu1);
+
+
+menu2=["Lemonade","Coke","tea","water"];
+print_array(menu2);
