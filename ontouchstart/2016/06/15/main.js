ed80f25f2c32bd7fcea99a4b98878ed0bc9f284a
Sam Liu <ontouchstart@gmail.com>
2016-06-15 17:18:16 -0400

    https://bigdata-mindstorms.github.io/d3-playground/ontouchstart/2016/06/15/
    Add border to code blocks.

--- ontouchstart/2016/06/15/main.js
+++ ontouchstart/2016/06/15/main.js
@@ -1,2 +1,7 @@
-d3.selectAll('pre').on('click', function () { eval(d3.select(this).text()); });
-
+d3.selectAll('pre')
+  .style('margin', '1em')
+  .style('padding', '1em')
+  .style('border', '1px solid #ccc')
+  .on('click', function () { 
+    eval(d3.select(this).text()); 
+  });
